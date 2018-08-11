cc972c48412ab64156a1142867b4f3aac830a447
Robert WC Seifert <rseifert.phone@gmail.com>
2018-08-11 04:29:51 -0400

    Added script to toggle collection area sprite
    Also remove collection cooldown as its makes no sense for gameplay.

---
+++ UnityProject/Assets/scripts/player/EnableCollectionSprite.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableCollectionSprite : MonoBehaviour 
+{
+    public CollectionArea pickupArea;
+    public SpriteRenderer spriteRenderer;
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		if(pickupArea.objectsInArea.Count > 0)
+        {
+            spriteRenderer.enabled = true; 
+        }
+        else
+        {
+            spriteRenderer.enabled = false; 
+        }
+	}
+}
--- UnityProject/Assets/scripts/player/PlayerCollectJunk.cs
+++ UnityProject/Assets/scripts/player/PlayerCollectJunk.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PlayerCollectJunk : MonoBehaviour 
 {
-    public float collectJunkCooldown = 1f;
     public float ropeConnectionDistance = 5;
     
     public GameObject junkConnectionPoint;
@@ -18,22 +17,13 @@ public class PlayerCollectJunk : MonoBehaviour
     //Junk collected
     public List<JunkObjectData> collectedJunk = new List<JunkObjectData>(); 
     
-    private float collectJunkTimer;
-    
 	// Update is called once per frame
 	void Update () 
     {
-        if(collectJunkTimer <= 0)
+        if(Input.GetButton("Fire2"))
         {
-            if(Input.GetButton("Fire2"))
-            {
-                collectJunkTimer = collectJunkCooldown;
-                CollectJunk();
-            }
-        }
-        else
-        {
-            collectJunkTimer -= Time.deltaTime;
+            collectJunkTimer = collectJunkCooldown;
+            CollectJunk();
         }
         
         foreach(JunkObjectData junkData in collectedJunk)
