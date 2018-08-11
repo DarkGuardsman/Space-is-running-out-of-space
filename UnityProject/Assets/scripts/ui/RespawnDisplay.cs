be4e39106181546095d5349168a34799d0071e09
Robert WC Seifert <rseifert.phone@gmail.com>
2018-08-11 06:54:01 -0400

    Created UI switcher and respawn UI handling

---
+++ UnityProject/Assets/scripts/ui/RespawnDisplay.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RespawnDisplay : UIDisplay   
+{
+    public Text respawnTimerText;
+    
+    void Update()
+    {
+        respawnTimerText.text = String.Format("{0:0}", gameController.respawnDelay - gameController.respawnTimer);
+    }	
+}
---
+++ UnityProject/Assets/scripts/ui/UISwitcher.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISwitcher : UIDisplay  
+{	
+    public Canvas gameUI;
+    public Canvas respawnUI;
+    public Canvas menuUI;
+    public Canvas optionsUI;
+    public Canvas helpUI;
+    public Canvas failedUI;
+    
+    public bool showMenu = false;    
+    public bool showHelp = false;
+    public bool showOptions = false;
+    
+	// Update is called once per frame
+	void Update () 
+    {
+        //disable all UIs
+        gameUI.enabled = false;
+        respawnUI.enabled = false;
+        //menuUI.enabled = false;
+        //optionsUI.enabled = false;
+        //helpUI.enabled = false;
+        //failedUI.enabled = false;
+        
+        //enable desired UI
+		if(showMenu)
+        {
+            
+        }
+        else if(showOptions)
+        {
+            
+        }
+        else if(showHelp)
+        {
+            
+        }
+        else if(gameController.gameOver)
+        {
+            
+        }
+        else if(gameController.respawnPlayer)
+        {
+            respawnUI.enabled = true;
+        }
+        else
+        {
+            gameUI.enabled = true;
+        }
+	}
+    
+    public void ShowMenu()
+    {
+        showMenu = true;
+    }
+    
+    public void ShowHelp()
+    {
+        showHelp = true;
+    }
+    
+    public void ShowOptions()
+    {
+        showOptions = true;
+    }
+}
