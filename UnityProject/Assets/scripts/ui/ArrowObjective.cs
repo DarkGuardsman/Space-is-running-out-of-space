c3967b8dcfb64a28fc0504ed34033b366a9ef438
Robert WC Seifert <rseifert.phone@gmail.com>
2018-08-12 08:05:20 -0400

    Implemented scaling for navigation arrows
    Arrows should grow in size the closer the player gets

--- UnityProject/Assets/scripts/ui/ArrowObjective.cs
+++ UnityProject/Assets/scripts/ui/ArrowObjective.cs
@@ -3,49 +3,75 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ArrowObjective : MonoBehaviour {
-
+public class ArrowObjective : UIDisplay 
+{
     public Transform aimTarget;
     public SpriteRenderer spriteRenderer;
     
     public float circleSize = 0.5f;
+    public float minScale = 0.3f;
+    public float scaleGrowth = 1f;
+    public float maxScale = 1f;
 	
 	// Update is called once per frame
 	void Update () 
     {
-        //https://answers.unity.com/questions/384074/how-to-make-a-2d-gui-arrow-point-at-a-3d-object.html
+        //Credit: https://answers.unity.com/questions/384074/how-to-make-a-2d-gui-arrow-point-at-a-3d-object.html
+        
+        //Disable sprite render
 		spriteRenderer.enabled = false; 
         
-        if(aimTarget != null)
+        if(aimTarget != null && gameController.currentPlayerObject != null)
         {         
-            Vector3 v3Pos = Camera.main.WorldToViewportPoint(aimTarget.transform.position);
-             
-            if (v3Pos.z < Camera.main.nearClipPlane)
-                return;  // Object is behind the camera
-             
-            if (v3Pos.x >= 0.0f && v3Pos.x <= 1.0f && v3Pos.y >= 0.0f && v3Pos.y <= 1.0f)
-                return; // Object center is visible
-                     
-            spriteRenderer.enabled = true; 
-            v3Pos.x -= 0.5f;  // Translate to use center of viewport
-            v3Pos.y -= 0.5f; 
-            v3Pos.z = 0;      // I think I can do this rather than do a 
-                               //   a full projection onto the plane
-             
-            float fAngle = Mathf.Atan2 (v3Pos.x, v3Pos.y);
-            transform.localEulerAngles = new Vector3(0.0f, 0.0f, -fAngle * Mathf.Rad2Deg);
-             
-            v3Pos.x = circleSize * Mathf.Sin (fAngle);  // Place on ellipse touching 
-            v3Pos.y = circleSize * Mathf.Cos (fAngle);  //   side of viewport
-            v3Pos.z = Camera.main.nearClipPlane + 0.01f;  // Looking from neg to pos Z;
+            //Get player and target position
+            Vector3 targetPosition = aimTarget.transform.position;
+            Vector3 playerPos = gameController.currentPlayerObject.transform.position;
             
+            //Convert target position to camera space
+            Vector3 pos = Camera.main.WorldToViewportPoint(targetPosition);
+            
+            //Get magnitude for scale
+            float distance = Mathf.Abs(Vector3.Distance(targetPosition, playerPos));
+             
+            //Check if behind camera
+            if (pos.z < Camera.main.nearClipPlane)
+                return;
+             
+            //Check if visible
+            if (pos.x >= 0.0f && pos.x <= 1.0f && pos.y >= 0.0f && pos.y <= 1.0f)
+                return;
+                     
+            //Enable render
+            spriteRenderer.enabled = true; 
+            
+            //Center
+            pos.x -= 0.5f;
+            pos.y -= 0.5f; 
+            pos.z = 0;
+            
+            //Set rotation towards target
+            float fAngle = Mathf.Atan2 (pos.x, pos.y);
+            transform.localEulerAngles = new Vector3(0.0f, 0.0f, -fAngle * Mathf.Rad2Deg);
+            
+            //Set position near edge of screen
+            pos.x = circleSize * Mathf.Sin (fAngle);  // Place on ellipse touching 
+            pos.y = circleSize * Mathf.Cos (fAngle);  //   side of viewport
+            pos.z = Camera.main.nearClipPlane + 0.01f;  // Looking from neg to pos Z;
+            
+            //Get camera size
             float height = Camera.main.orthographicSize * 2f;
             float width = height * Camera.main.aspect;
             
-            v3Pos.x *= width;
-            v3Pos.y *= height;
+            //Scale position to camera size
+            pos.x *= width;
+            pos.y *= height;
             
-            transform.localPosition = v3Pos;
+            //Set position
+            transform.localPosition = pos;
+            
+            //Scale based on distance
+            float scale = minScale + (maxScale - Mathf.Min(distance * scaleGrowth, maxScale));
+            transform.localScale = new Vector3( scale, scale, 1);
         }
 	}
 }
