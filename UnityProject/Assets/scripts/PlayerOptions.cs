2ee0ee79cddb75cb11df344d0ba301a8da0dc708
Robert WC Seifert <rseifert.phone@gmail.com>
2018-08-12 09:15:54 -0400

    Created settings for arrow icon min/max size and junk spawn limit

---
+++ UnityProject/Assets/scripts/PlayerOptions.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOptions : MonoBehaviour 
+{
+	public float arrowMinScale;
+    public float arrowMaxScale;
+    public int maxJunkSpawn;
+    
+    void Awake()
+    {
+        LoadOptions();
+    }
+    
+	public void LoadOptions () 
+    {
+        Debug.Log("PlayerOptions: Loading player prefs");
+        if(PlayerPrefs.HasKey("arrowMinScale"))
+        {
+            arrowMinScale = PlayerPrefs.GetFloat("arrowMinScale");
+            Debug.Log("PlayerOptions: Arrow Min -> " + arrowMinScale);
+        }
+        if(PlayerPrefs.HasKey("arrowMaxScale"))
+        {
+            arrowMaxScale = PlayerPrefs.GetFloat("arrowMaxScale");
+            Debug.Log("PlayerOptions: Arrow Max -> " + arrowMaxScale);
+        }
+        if(PlayerPrefs.HasKey("maxJunkSpawn"))
+        {
+            maxJunkSpawn = PlayerPrefs.GetInt("maxJunkSpawn");
+            Debug.Log("PlayerOptions: Junk Count -> " + maxJunkSpawn);
+        }
+	}	
+	
+	public void SaveOptions () 
+    {
+        Debug.Log("PlayerOptions: Saving player prefs");
+        PlayerPrefs.SetFloat("arrowMinScale", arrowMinScale);
+        PlayerPrefs.SetFloat("arrowMaxScale", arrowMaxScale);
+        PlayerPrefs.SetInt("maxJunkSpawn", maxJunkSpawn);
+	}
+}
---
+++ UnityProject/Assets/scripts/ui/UIOptions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIOptions : UIDisplay 
+{
+    public Slider minArrowSizeSlider;
+    public Slider maxArrowSizeSlider;
+    public Slider maxJunkCountSlider;    
+    
+    public InputField minArrowSizeInput;
+    public InputField maxArrowSizeInput;
+    public InputField maxJunkCountInput;  
+    
+    public float minAllowArrowMaxSize = 1f;
+    public float maxAllowedArrowMaxSize = 3f;
+    
+    public float minAllowArrowMinSize = 0.3f;
+    public float maxAllowedArrowMinSize = 1f;
+    
+    public int minJunkSpawn = 20;
+    public int maxJunkSpawn = 1000;
+    
+    private PlayerOptions playerOptions;
+    
+	// Use this for initialization
+	public override void Start () 
+    {
+		base.Start();
+        playerOptions = FindObjectOfType<PlayerOptions>();
+    }
+    
+    void FixedUpdate()
+    {
+        minArrowSizeSlider.value = Mathf.Min(1, (playerOptions.arrowMinScale - minAllowArrowMinSize) / (maxAllowedArrowMinSize - minAllowArrowMinSize));
+        maxArrowSizeSlider.value = Mathf.Min(1, (playerOptions.arrowMaxScale - minAllowArrowMaxSize) / (maxAllowedArrowMaxSize - minAllowArrowMaxSize));
+        maxJunkCountSlider.value = Mathf.Min(1, (((float)playerOptions.maxJunkSpawn) - minJunkSpawn) / (maxJunkSpawn - minJunkSpawn));
+	}
+    
+    public void ButtonSave()
+    {
+        playerOptions.arrowMinScale = float.Parse(minArrowSizeInput.text);
+        playerOptions.arrowMaxScale = float.Parse(maxArrowSizeInput.text);
+        playerOptions.maxJunkSpawn = int.Parse(maxJunkCountInput.text);
+        playerOptions.SaveOptions();
+    }
+    
+    public void OnSliderChanged()
+    {
+        float minArrowSize = minAllowArrowMinSize + (maxAllowedArrowMinSize - minAllowArrowMinSize) * minArrowSizeSlider.value;
+        float maxArrowSize = minAllowArrowMaxSize + (maxAllowedArrowMaxSize - minAllowArrowMaxSize) * maxArrowSizeSlider.value;
+        float junkSpawn = minJunkSpawn + (maxJunkSpawn - minJunkSpawn) * maxJunkCountSlider.value;
+        
+        if(maxArrowSize <= minArrowSize)
+        {
+            maxArrowSize = minArrowSize + 0.1f;
+        }
+        
+        minArrowSizeInput.text = String.Format("{0}", minArrowSize);
+        maxArrowSizeInput.text = String.Format("{0}", maxArrowSize);
+        maxJunkCountInput.text = String.Format("{0:0}", junkSpawn);
+    }
+}
