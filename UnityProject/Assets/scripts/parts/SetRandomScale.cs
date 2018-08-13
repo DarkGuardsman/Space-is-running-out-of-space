76a5115a5fa0d71ca3c3f6bc33d456be8a34f1bc
Robert WC Seifert <rseifert.phone@gmail.com>
2018-08-13 05:33:16 -0400

    Added random scale to junk

---
+++ UnityProject/Assets/scripts/parts/SetRandomScale.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetRandomScale : MonoBehaviour 
+{
+    public float scale = 0.2f;
+    
+	// Use this for initialization
+	void Start () 
+    {
+        gameObject.transform.localScale += new Vector3(Random.Range(-scale, scale), Random.Range(-scale, scale), 0);
+        
+		//Disable as this only runs once
+        enabled = false;
+	}
+}
