d42cba0aaf718d2ecb7404fcb2a6f5846d0282d5
Robert WC Seifert <rseifert.phone@gmail.com>
2018-08-13 00:13:04 -0400

    Added zoom

--- UnityProject/Assets/scripts/PlayerOptions.cs
+++ UnityProject/Assets/scripts/PlayerOptions.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerOptions : MonoBehaviour 
 {
 	public float arrowMinScale;
     public float arrowMaxScale;
+    public float cameraZoom = 8f;
+    public float zoomSpeed = 0.1f;
     public int maxJunkSpawn;
+    
+    private GameController gameController;
+    private CinemachineVirtualCamera cinemachineCamera;
+    
+    // Use this for initialization
+	void Start () 
+    {
+		gameController = FindObjectOfType<GameController>();
+        cinemachineCamera = FindObjectOfType<CinemachineVirtualCamera>();
+
+        ApplyOptions();
+	}
     
     void Awake()
     {
-        LoadOptions();
+        LoadOptions();        
     }
     
 	public void LoadOptions () 
@@ -31,6 +46,11 @@ public class PlayerOptions : MonoBehaviour
             maxJunkSpawn = PlayerPrefs.GetInt("maxJunkSpawn");
             Debug.Log("PlayerOptions: Junk Count -> " + maxJunkSpawn);
         }
+        if(PlayerPrefs.HasKey("cameraZoom"))
+        {
+            cameraZoom = PlayerPrefs.GetFloat("cameraZoom");
+            Debug.Log("PlayerOptions: Camera Zoom -> " + maxJunkSpawn);
+        }
 	}	
 	
 	public void SaveOptions () 
@@ -39,5 +59,15 @@ public class PlayerOptions : MonoBehaviour
         PlayerPrefs.SetFloat("arrowMinScale", arrowMinScale);
         PlayerPrefs.SetFloat("arrowMaxScale", arrowMaxScale);
         PlayerPrefs.SetInt("maxJunkSpawn", maxJunkSpawn);
+        PlayerPrefs.SetFloat("cameraZoom", cameraZoom);
+        
+        //Update options, needed for option menu save button
+        ApplyOptions();
 	}
+    
+    public void ApplyOptions()
+    {
+        //Update settings
+        cinemachineCamera.m_Lens.OrthographicSize = cameraZoom;
+    }
 }
---
+++ UnityProject/Assets/scripts/player/PlayerZoom.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class PlayerZoom : MonoBehaviour 
+{    
+    public float minZoom = 5;
+    public float maxZoom = 20;
+    
+    private GameController gameController;
+    private PlayerOptions playerOptions;
+    private CinemachineVirtualCamera cinemachineCamera;
+    
+    // Use this for initialization
+	void Start () 
+    {
+		gameController = FindObjectOfType<GameController>();
+        playerOptions = FindObjectOfType<PlayerOptions>();
+        cinemachineCamera = FindObjectOfType<CinemachineVirtualCamera>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        //Get input
+        float zoom = Input.GetAxis("Zoom");
+        
+        //Do zoom
+        cinemachineCamera.m_Lens.OrthographicSize -= playerOptions.zoomSpeed * zoom;
+        
+        //Clamp
+        if(cinemachineCamera.m_Lens.OrthographicSize > maxZoom)
+        {
+            cinemachineCamera.m_Lens.OrthographicSize = maxZoom;
+        }
+        else if(cinemachineCamera.m_Lens.OrthographicSize < minZoom)
+        {
+            cinemachineCamera.m_Lens.OrthographicSize = minZoom;
+        }
+	}
+}
