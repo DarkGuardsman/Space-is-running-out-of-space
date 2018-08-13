20999cc5a357a964b208679a1d1a2f1034afba6a
Robert WC Seifert <rseifert.phone@gmail.com>
2018-08-13 08:52:41 -0400

    Added fps counter

---
+++ UnityProject/Assets/scripts/ui/FPSDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSDisplay : MonoBehaviour 
+{
+    public Text text;
+    float deltaTime = 0.0f;
+    float fps = 60;
+ 
+	void Update()
+	{
+		deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+        fps = 1.0f / deltaTime;
+        
+        text.text = String.Format("FPS: {0:0}", fps);
+	}
+}
