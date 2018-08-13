15e38b80709c5852cb9f23e5f14eb9e1f7568607
Robert WC Seifert <rseifert.phone@gmail.com>
2018-08-13 02:49:59 -0400

    Added script to set junk into motion on spawn

---
+++ UnityProject/Assets/scripts/parts/GenerateRandomMotion.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateRandomMotion : MonoBehaviour 
+{
+    public float randomMovementEnergy = 100f;
+    public float randomRotationEnergy = 5f;
+    
+	// Use this for initialization
+	void Start () 
+    {
+		Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
+        
+        if(rb != null)
+        {
+            Vector2 force = new Vector2(Random.Range(-randomMovementEnergy, randomMovementEnergy), Random.Range(-randomMovementEnergy, randomMovementEnergy));
+            rb.AddForce(force, ForceMode2D.Impulse);
+            
+            float torque = Random.Range(-randomRotationEnergy, randomRotationEnergy);
+            rb.AddTorque(torque, ForceMode2D.Impulse);
+        }
+        else
+        {
+            Debug.LogError("GenerateRandomMotion script requires a rigidbody2D to work");
+        }
+         
+        
+        //Disable as this only runs once
+        enabled = false;
+	}
+}
