13c6adb9d1fb5eb9dcf0d3972616d80d6c8eaba0
Robert WC Seifert <rseifert.phone@gmail.com>
2018-08-25 15:56:32 -0400

    Added tab selector support to restart and exit dialogs

--- UnityProject/Assets/scripts/ui/UIMenu.cs
+++ UnityProject/Assets/scripts/ui/UIMenu.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class UIMenu : UIDisplay 
 {
 	public UISwitcher uiSwitcher;
+    
     public UITabSelector menuSelector;
+    
     public Canvas canvus;
     
     public GameObject mainUIPanel;
