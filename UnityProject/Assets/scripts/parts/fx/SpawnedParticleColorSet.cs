3d96dcdf492a9b3b3ecce49c35fa6a2b3eb19d63
Robert WC Seifert <rseifert.phone@gmail.com>
2018-09-07 09:13:30 -0400

    Created speed and color effect script
    Color script will assign a particle color from the collision object. 
    
    Speed will try to scale particle count to collision speed. It needs more work as its not having the desired effect. Mainly there is not a massive visual difference. Which will need to be solved by adjusting how the particles spawn.
    
    There also seems to be an issue were the effect spawn on a different location from the collision spot.

---
+++ UnityProject/Assets/scripts/parts/fx/SpawnedParticleColorSet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnedParticleColorSet : SpawnedEffect 
+{
+	public override void OnSpawnedCollision(GameObject host, GameObject collisionObject, CollisionDamage collisionDamage,  float collisionSpeed)
+    {
+        ParticleSystem particleSystem = GetComponent<ParticleSystem>();
+        if(particleSystem != null)
+        {
+            //TODO average all sprites
+            //TODO maybe add a more exact way to get the color
+            SpriteRenderer spriteRenderer = host.GetComponentInChildren<SpriteRenderer>();
+            if(spriteRenderer != null)
+            {
+                ParticleSystem.MainModule main = particleSystem.main;
+                main.startColor = spriteRenderer.color;
+            }
+            else
+            {
+                Debug.Log("SpawnedParticleColorSet: Failed to get sprite render from " + host);
+            }
+        }
+        else
+        {
+            Debug.Log("SpawnedParticleColorSet: Failed to find particle system on " + gameObject);
+        }
+    }
+}
---
+++ UnityProject/Assets/scripts/parts/fx/SpawnedParticleCountWithSpeed.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnedParticleCountWithSpeed : SpawnedEffect 
+{
+    public float minCount = 10;
+    public float maxCount = 40;
+    
+	public override void OnSpawnedCollision(GameObject host, GameObject collisionObject, CollisionDamage collisionDamage,  float collisionSpeed)
+    {
+        ParticleSystem particleSystem = GetComponent<ParticleSystem>();
+        if(particleSystem != null)
+        {
+            float count = Mathf.Min(minCount, collisionDamage.ScaleForSpeed(collisionSpeed) * maxCount);
+            
+            ParticleSystem.EmissionModule emission = particleSystem.emission;
+            emission.rateOverTime = count;
+            particleSystem.maxParticles = (int)count;
+        }
+        else
+        {
+            Debug.Log("SpawnedParticleColorSet: Failed to find particle system on " + gameObject);
+        }
+    }
+}
