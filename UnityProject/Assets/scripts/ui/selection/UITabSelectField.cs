2f01c5a196b62330f883ab9e092b44e02f14bec2
Robert WC Seifert <rseifert.phone@gmail.com>
2018-09-08 07:22:35 -0400

    Implemented tab selection for text fields

---
+++ UnityProject/Assets/scripts/ui/selection/UITabSelectField.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UITabSelectField : UISelectionObject 
+{
+    public InputField inputField;
+    
+    void Start()
+    {
+        inputField = GetComponent<InputField>();
+    }
+
+	public override bool OnSelected(UITabSelector selector, bool forward)
+    {
+        inputField.Select();
+        inputField.ActivateInputField();
+        return false;
+    }
+    
+    public override void OnUnSelected(UITabSelector selector)
+    {
+        inputField.DeactivateInputField();
+        inputField.OnDeselect(new BaseEventData(EventSystem.current));
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+    
+    public override void OnSelectorEnabled(UITabSelector selector, bool enabled)
+    {
+        inputField.DeactivateInputField();
+        inputField.OnDeselect(new BaseEventData(EventSystem.current));
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+}
